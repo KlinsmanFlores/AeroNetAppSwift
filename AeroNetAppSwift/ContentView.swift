cded0b1d423dcfb6171a8dffa4b2b099476c548e
KlinsmanFlores <klinsman1597@hotmail.com>
2026-06-21 08:11:47 -0700

    Initial Commit

---
+++ AeroNetAppSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AeroNetAppSwift
+//
+//  Created by KlinsmanFlores on 21/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
