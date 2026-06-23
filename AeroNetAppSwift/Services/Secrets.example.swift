1a9d23250979b2bb791f21819bcdb3c8b62f6124
KlinsmanFlores <klinsman1597@hotmail.com>
2026-06-22 22:04:02 -0500

    a8

--- AeroNetAppSwift/Services/Secrets.example.swift
+++ AeroNetAppSwift/Services/Secrets.example.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-struct Secrets {
+// Este archivo es solo un ejemplo. No se usa en la compilación real.
+// Para configurar tu entorno local, duplica este archivo como "Secrets.swift" y cambia la URL.
+// Cambiamos el nombre de la estructura aquí a "SecretsExample" para que no choque con tu Secrets real.
+struct SecretsExample {
     // Ejemplo de configuración para desarrollo.
     // Los desarrolladores deben duplicar este archivo y renombrarlo a "Secrets.swift"
     static let apiBaseURL = "http://localhost:3000/api"
